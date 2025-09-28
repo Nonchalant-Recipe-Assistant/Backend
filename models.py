1405aa2a650e319eb479736d2b0cdebd439c95b4
ALIAKSEI SYCHOU <144005377+Edlws@users.noreply.github.com>
2025-09-28 17:14:10 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, TIMESTAMP, func
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Role(Base):
+    __tablename__ = "roles"
+    role_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+
+    users = relationship("User", back_populates="role")
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key=True, index=True)
+    role_id = Column(Integer, ForeignKey("roles.role_id"), nullable=False)
+    email = Column(String(100), unique=True, nullable=False, index=True)
+    password_hash = Column(String(255), nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    role = relationship("Role", back_populates="users")
